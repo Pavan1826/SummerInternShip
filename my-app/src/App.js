49940136741e6df0f1292dd2a8881ff30053f607
Pavan <pajhagadiawala@gmail.com>
2023-06-02 14:26:33 +0530

    Project

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,93 +1,41 @@
-// import logo from './images/logo.svg';
-// import Sitelogo from '../public/logo192.png';
-import './App.css';
-import {Routes, Route, Link, BrowserRouter} from 'react-router-dom';
-// import { globalStyles } from './Components/constants';
-import {HomePage} from './Pages/HomePage';
-import {Apple} from './Pages/OlRegistration';
-import Registration from './Pages/Registration';
-import Login from './Pages/Login';
-import {NotFound} from './Pages/NotFound';
-import {ThemeProvider} from '@emotion/react';
-import {theme} from './style';
-import HomeIcon from '@mui/icons-material/Home';
-import HowToRegIcon from '@mui/icons-material/HowToReg';
-import LoginIcon from '@mui/icons-material/Login';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import Navbar from './components/Header/Header';
-import Footer from './components/Footer/Footer';
+import React from "react";
+import { ThemeProvider } from "@material-ui/core/styles";
+import { theme } from "./utils/theme";
+import "./assets/css/style.css";
+import Header from "./components/header/index";
+import Footer from "./components/footer/index";
+import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import MainNavigation from "./components/MainNavigation";
+import { AuthWrapper } from "./context/auth";
+import loader from "./assets/images/loader.gif";
+import { CartWrapper } from "./context/cart";
 
-
-const App = () => { 
-    return ( 
-    <> 
+const App = () => {
+  return (
     <ThemeProvider theme={theme}>
-    
-        <ToastContainer/>
-       
+      <React.Suspense fallback={<></>}>
         <BrowserRouter>
-        <Navbar/>
-            {/* <div className='nav' style={{
-                display:'flex',
-                alignItems: 'center'
-            }}>
-                <Link to='/' style={{
-                    marginLeft: 5,
-                    textDecoration:'none',
-                    fontSize:20,
-                    color:'#E76161',
-                    display:'flex',
-                    alignItems: 'center'
-                }}>
-                    <HomeIcon/>
-                    Home</Link>
-                <Link
-                    to='/Registration'
-                    style={{
-                    marginLeft: 10,
-                    textDecoration:'none',
-                    fontSize:20,
-                    color:'#E76161',
-                    display:'flex',
-                    alignItems: 'center'
-                }}>
-                    <HowToRegIcon/>
-                    Registration</Link>
-                <Link
-                    to='/Login'
-                    style={{
-                    marginLeft: 10,
-                    textDecoration:'none',
-                    fontSize:20,
-                    color:'#E76161',
-                    display:'flex',
-                    alignItems: 'center'
-                }}>
-                    <LoginIcon/>
-                    Login</Link>
-                <Link
-                    to='/applet'
-                    style={{
-                    marginLeft: 12,
-                    textDecoration:'none',
-                    fontSize:20,
-                    color:'#E76161'
-                }}>
-                    Applet</Link>
-            </div> */}
-            <Routes >
-                <Route path='/' element={< HomePage />}></Route>
-                <Route path='/Registration' element={< Registration />}></Route>
-                <Route path='/Login' element={< Login />}></Route>
-                <Route path='*' element={< NotFound />}></Route>
-
-            </Routes>
-            {/* <Footer/> */}
+          <AuthWrapper>
+            <CartWrapper>
+              <div className="loader-wrapper">
+                <img src={loader} alt="loader" />
+              </div>
+              <div className="wrapper">
+                <Header />
+                <main>
+                  <MainNavigation />
+                </main>
+                <Footer />
+              </div>s
+              <ToastContainer />
+            </CartWrapper>
+          </AuthWrapper>
         </BrowserRouter>
-
-    </ThemeProvider> 
-    </>
+      </React.Suspense>
+    </ThemeProvider>
   );
 };
+
 export default App;
